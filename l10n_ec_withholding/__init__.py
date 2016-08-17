63f6cab67a5b92b85efd8c3453a87539189ed02e
Cristian Salamea <ovnicraft@gmail.com>
2016-08-17 14:27:56 -0500

    migracion de retenciones
    restructura de directorio, revisar eliminacion de tax_group para dar paso al account.tax.group de odoo y se agrega un codigo que deberemos cargar un listado de grupos para la asignacion a los impuestos

--- l10n_ec_withholding/__init__.py
+++ l10n_ec_withholding/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import product
-from . import account
-from . import withdrawing
-from . import wizard
-from . import report
+from . import models
+#from . import wizard
+#from . import report
