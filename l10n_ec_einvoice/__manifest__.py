499cc829d732c9bd04d415874e9e5de23d8cb208
Cristian Salamea <ovnicraft@gmail.com>
2017-01-16 15:43:31 -0500

    [QA]
    removidos archivos sin usar, licencias, permisos

--- l10n_ec_einvoice/__manifest__.py
+++ l10n_ec_einvoice/__manifest__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Electronic Documents for Ecuador',
-    'version': '0.1.0',
+    'version': '10.0.0.1.0',
     'author': 'Cristian Salamea',
     'category': 'Localization',
+    'license': 'AGPL-3',
     'complexity': 'normal',
     'data': [
         'security/ir.model.access.csv',
@@ -17,8 +18,5 @@
     ],
     'depends': [
         'l10n_ec_withholding',
-    ],
-    'description': '''
-    This module allows generate Electronic Documents for Ecuador
-    '''
+    ]
 }
